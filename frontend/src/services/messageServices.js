b586cfde6be6c212db37c7a0609df15e5058095a
Luca Valenziano <38068415+LucaMicroscope@users.noreply.github.com>
2026-08-19 17:15:23 +0200

    Update messageServices.js
    Fix

--- frontend/src/services/messageServices.js
+++ frontend/src/services/messageServices.js
@@ -1,4 +1,4 @@
-//Servizio che si occupa delle chiamate alle API del ChatController getMessages,createMessage,updateMessage,deleteMessage
+//Servizio che si occupa delle chiamate alle API del ChatController
 
 import axios from '../axios'
 
@@ -13,11 +13,11 @@ export async function createMessage(chatId, data) {
 }
 
 export async function updateMessage(chatId, messageId, data) {
-    const response = await axios.post(`/chats/${chatId}/messages/${messageId}`, data)
+    const response = await axios.put(`/chats/${chatId}/messages/${messageId}`, data)
     return response.data
 }
 
 export async function deleteMessage(chatId, messageId) {
-    const response = await axios.post(`/chats/${chatId}/messages/${messageId}`)
+    const response = await axios.delete(`/chats/${chatId}/messages/${messageId}`)
     return response.data
 }
