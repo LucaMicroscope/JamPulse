809bb62315ef8d11fdb6bdb0e02f3442074194a7
Luca Valenziano <38068415+LucaMicroscope@users.noreply.github.com>
2026-08-20 18:09:42 +0200

    Protected Route
    Usando l'AuthContext ho creato la funzione protected route:
    Se l'utente non è loggato e prova ad accedere alle pagine protette, verrà reindirizzato alla pagina di login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,6 +8,16 @@ import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import CreatePost from "./pages/CreatePost";
 import PageLayout from "./components/PageLayout";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth()
+  // Se l'utente non è loggato, lo cacciamo e lo mandiamo al login
+  if (!user)
+    return <Navigate to='/login' replace /> //replace serve per modificare la cronologia e mostrare anche li solo la pagina Login invece di Home -> Login
+  // Se è loggato, gli mostriamo normalmente la pagina richiesta
+  return children
+
+}
 
 // Componente principale dell'applicazione.
 // Qui vengono definite le route principali e il layout condiviso che racchiude le pagine interne.
@@ -18,7 +29,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
 
         {/* Tutte le pagine interne condividono il layout comune: la Sidebar. */}
-        <Route element={<PageLayout />}>
+        <Route element={<ProtectedRoute><PageLayout /></ProtectedRoute>}>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/profile" element={<Profile />} />
