a6e61d709185077157b16546244ac4d8abf4b3b2
Luca Valenziano <38068415+LucaMicroscope@users.noreply.github.com>
2026-08-24 16:14:34 +0200

    Update PostCard.jsx
    Ho spostato la funzione getTimeAgo che prima si trovava nel componente PostCard, in modo da poterla usare anche su altri componenti.
    Questa funzione calcola quanto tempo fa è stato pubblicato un post/commento e mostra solo il numero intero più una lettera per indicare secondi,minuti,ore o giorni

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -16,26 +16,6 @@ import { deletePost } from "../services/postServices";
 //   createdAt: "..."
 // }
 
-// Piccola funzione di supporto per calcolare il tempo trascorso
-// Converte la data del post in stringhe come "1h", "2d", "5m" 
-function getTimeAgo(dateString) {
-    if (!dateString) return '';
-    const date = new Date(dateString);
-    const now = new Date();
-    const diffInSeconds = Math.floor((now - date) / 1000);
-
-    if (diffInSeconds < 60) return `${diffInSeconds}s`;
-
-    const diffInMinutes = Math.floor(diffInSeconds / 60);
-    if (diffInMinutes < 60) return `${diffInMinutes}m`;
-
-    const diffInHours = Math.floor(diffInMinutes / 60);
-    if (diffInHours < 24) return `${diffInHours}h`;
-
-    const diffInDays = Math.floor(diffInHours / 24);
-    return `${diffInDays}d`;
-}
-
 export default function PostCard({ post, onDelete }) {
     const navigate = useNavigate();
 
