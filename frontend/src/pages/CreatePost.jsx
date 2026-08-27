775e78e1d1dc45f1568f13b453d16a3a2597b3e7
Luca Valenziano <38068415+LucaMicroscope@users.noreply.github.com>
2026-08-27 11:42:23 +0200

    Update CreatePost.jsx
    modificato solo il colore il weigth di "Crea un nuovo post"

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,5 +1,4 @@
-import { Alert, Button, Card, CircularProgress, Stack, TextField, Typography } from "@mui/material";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Alert, Button, Card, CircularProgress, Stack, TextField, Typography, } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createPost } from "../services/postServices";
@@ -62,7 +61,7 @@ export default function CreatePost() {
                     specialmente con i Dialog. È più idiomatico gestire l'invio
                     con onSubmit direttamente sullo Stack che funge da contenitore. */}
                 <Stack spacing={5} component="form" onSubmit={handleSubmit}>
-                    <Typography variant="h5" align="center">
+                    <Typography variant="h5" align="center" color="primary" sx={{ fontWeight: 'bold' }}>
                         Crea un nuovo post
                     </Typography>
 
