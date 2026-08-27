2557eb1a6ba957e9582ad189688ec3584fde91ba
Luca Valenziano <38068415+LucaMicroscope@users.noreply.github.com>
2026-08-27 18:11:25 +0200

    Create musicOptions.js
    Questo componente serve a rendere accessibili gli array di strumenti e generi, senza doverli scrivere ogni volta

---
+++ frontend/src/utils/musicOptions.js
@@ -0,0 +1,26 @@
+// Esportiamo le costanti in modo che possano essere importate in qualsiasi componente
+
+export const instruments = [
+    "Arpa", 
+    "Basso", 
+    "Batteria", 
+    "Chitarra", 
+    "Pianoforte", 
+    "Sassofono",
+    "Sintetizzatore",
+    "Tastiera",
+    "Voce"
+];
+
+export const genres = [
+    "Blues", 
+    "Classica", 
+    "Elettronica",
+    "Funk", 
+    "Hip Hop", 
+    "Indie",
+    "Jazz", 
+    "Metal", 
+    "Pop", 
+    "Rock" 
+];
